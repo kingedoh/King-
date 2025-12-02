10d36b1198b6c2b0541092ef5ce8eb6576a7bfe2
kingedoh <kingedoh2012@gmail.com>
2025-12-02 16:35:32 +0100

    Implement toggleContent function in script.js
    Add toggleContent function to show/hide elements

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+```js
+function toggleContent(id) {
+  const elem = document.getElementById(id);
+  if (elem.style.display === 'block') {
+    elem.style.display = 'none';
+  } else {
+    elem.style.display = 'block';
+  }
+}
+```
+
+That’s your full dark & classy portfolio web app, King 👑!  
+Anything else you want to add or tweak?
